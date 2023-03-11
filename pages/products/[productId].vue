d8603a9e58b09199b261f3d63dc3046d9408639f
Gergely Kiss <gergely.kiss@r34dy.io>
2023-03-11 17:59:39 +0100

    different look and feel

--- pages/products/[productId].vue
+++ pages/products/[productId].vue
@@ -6,12 +6,14 @@
       class="w-96 h-96 object-contain"
     />
     <div class="flex flex-col gap-5">
-      <p class="font-bold text-gray-500 block w-full truncate">
+      <p class="font-bold text-gray-500 text-4xl block w-full truncate">
         {{ product.title }}
       </p>
-      <p class="max-w-lg">{{ product.description }}</p>
-      <p>{{ product.category }}</p>
-      <p class="text-[#12b488] font-bold text-lg">{{ product.price }} $</p>
+      <p class="text-[#12b488] font-bold text-lg">
+        Price: {{ product.price }} $
+      </p>
+      <p class="font-bold border-b-2">Product description:</p>
+      <p>{{ product.description }}</p>
       <NuxtLink :to="`/products`" class="btn">Buy</NuxtLink>
     </div>
   </div>
