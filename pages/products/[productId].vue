87eeed079bb3f390122905a66afe11c15752381a
Gergely Kiss <gergely.kiss@r34dy.io>
2023-03-11 17:54:57 +0100

    item details page

--- pages/products/[productId].vue
+++ pages/products/[productId].vue
@@ -1,21 +1,29 @@
 <template>
-  <div>Product by {{ product.id }}</div>
-  <div>
-    {{ product.title }}
-  </div>
-  <div>
-    {{ product.price }}
-  </div>
-  <div>
-    {{ product.id }}
+  <div v-if="product" class="flex flex-col md:flex-row gap-10">
+    <img
+      :src="product.image"
+      alt="product thumb"
+      class="w-96 h-96 object-contain"
+    />
+    <div class="flex flex-col gap-5">
+      <p class="font-bold text-gray-500 block w-full truncate">
+        {{ product.title }}
+      </p>
+      <p class="max-w-lg">{{ product.description }}</p>
+      <p>{{ product.category }}</p>
+      <p class="text-[#12b488] font-bold text-lg">{{ product.price }} $</p>
+      <NuxtLink :to="`/products`" class="btn">Buy</NuxtLink>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { IProduct } from "~~/interfaces/product";
+
 const { productId } = useRoute().params;
-const { data: product } = await useFetch(
+const { data: product } = await useFetch<IProduct>(
   `https://fakestoreapi.com/products/${productId}`,
-  { key: productId }
+  { key: productId as string }
 );
 
 definePageMeta({ layout: "products" });
